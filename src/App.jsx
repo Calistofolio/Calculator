a0cf860efccd8b031c93db897e3b63381fe553f1
Luana Calisto <luanavenancio060@gmail.com>
2025-01-24 10:44:19 -0300

    implements "add value", "equals" and "clean" functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,62 @@
+import { useState } from "react"
 import Button from "./components/button/button"
 
 function App() {
 
-  const row1 = ["AC", "+/-" ,"%", "/"]
-  const row2 = [7, 8 , 9, "X"]
-  const row3 = [4, 5 , 6, "-"]
-  const row4 = [1, 2 , 3, "+"]
-  const row5 = [0, ",", "="]
+  const [input, setInput] = useState() 
+  const [result, setResult] = useState()
+
+  const row1 = ["+/-" ,"%", "/"]
+  const row2 = ["7", "8" , "9", "X"]
+  const row3 = ["4", "5" , "6", "-"]
+  const row4 = ["1", "2" , "3", "+"]
+  const row5 = ["0", ","]
+
+  function addValue(value) {
+    if (input == undefined || input == " ") {
+      setInput(value)
+    } else{
+      setInput((prev) => prev + value)
+    }
+  }
+
+  function equals() {
+    const result = eval(input)
+    setInput(result)
+  }
+
+  function clean(){
+    setInput(" ")
+  }
+
+  
+
+ 
 
 
   return (
     <div>
-      {row1.map((n) => <Button title = {n}/>)}
+    <div>
+      <input type="text" value={input}/>
+    </div>
+    <div>
+      <Button title = "AC" onclick = {() => clean()}/>
+      {row1.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
       <div>
-        {row2.map((n) => <Button title = {n}/>)}
+        {row2.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
       </div>
       <div>
-        {row3.map((n) => <Button title = {n}/>)}
+        {row3.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
       </div>
       <div>
-        {row4.map((n) => <Button title = {n}/>)}
+        {row4.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
       </div>
       <div>
-        {row5.map((n) => <Button title = {n}/>)}
+        {row5.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
+        <Button title = "=" onclick = {() => equals()}/>
+        
       </div>
+    </div>
     </div>
   )
 }
