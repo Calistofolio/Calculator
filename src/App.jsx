abfa602f9b470e0cff8ed8fc7a3af1b9a855fc01
Luana Calisto <luanavenancio060@gmail.com>
2025-01-24 10:59:37 -0300

    creates calculator component

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,10 @@
-import { useState } from "react"
-import Button from "./components/button/button"
+import Calculator from "./components/calculator/calculator"
 
 function App() {
 
-  const [input, setInput] = useState() 
-  const [result, setResult] = useState()
-
-  const row1 = ["+/-" ,"%", "/"]
-  const row2 = ["7", "8" , "9", "X"]
-  const row3 = ["4", "5" , "6", "-"]
-  const row4 = ["1", "2" , "3", "+"]
-  const row5 = ["0", ","]
-
-  function addValue(value) {
-    if (input == undefined || input == " ") {
-      setInput(value)
-    } else{
-      setInput((prev) => prev + value)
-    }
-  }
-
-  function equals() {
-    const result = eval(input)
-    setInput(result)
-  }
-
-  function clean(){
-    setInput(" ")
-  }
-
-  
-
- 
-
-
   return (
     <div>
-    <div>
-      <input type="text" value={input}/>
-    </div>
-    <div>
-      <Button title = "AC" onclick = {() => clean()}/>
-      {row1.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
-      <div>
-        {row2.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
-      </div>
-      <div>
-        {row3.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
-      </div>
-      <div>
-        {row4.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
-      </div>
-      <div>
-        {row5.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
-        <Button title = "=" onclick = {() => equals()}/>
-        
-      </div>
-    </div>
+      <Calculator></Calculator>
     </div>
   )
 }
