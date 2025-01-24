439ecf30941fed8e33418c0183b7657fecf260c2
Luana Calisto <luanavenancio060@gmail.com>
2025-01-24 13:38:09 -0300

    changes buttons and centers calculator

--- src/components/calculator/calculator.jsx
+++ src/components/calculator/calculator.jsx
@@ -1,21 +1,28 @@
 import { useState } from "react"
 import Button from "../button/button.jsx"
+import "./calculator.css"
 
 function Calculator() {
 
-  const [input, setInput] = useState() 
+  const [input, setInput] = useState(" ") 
+  const [perInput, setPerInput] = useState(" ")
 
-  const row1 = ["+/-" ,"%", "/"]
-  const row2 = ["7", "8" , "9", "*"]
-  const row3 = ["4", "5" , "6", "-"]
-  const row4 = ["1", "2" , "3", "+"]
+  const row2 = ["7", "8" , "9"]
+  const row3 = ["4", "5" , "6"]
+  const row4 = ["1", "2" , "3"]
   const row5 = ["0", ","]
 
   function addValue(value) {
     if (input == undefined || input == " ") {
       setInput(value)
-    } else{
+    } else {
       setInput((prev) => prev + value)
+    }
+
+    if(input == undefined || input == " " && value == "/" 
+        || input == undefined || input == " " && value == "+" 
+        || input == undefined || input == " " && value == "*"){
+        setInput(" ")
     }
   }
 
@@ -28,28 +35,34 @@ function Calculator() {
     setInput(" ")
   }
 
+
   return (
-    <div>
-    <div>
-      <input type="text" value={input}/>
-    </div>
-    <div>
-      <Button title = "AC" onclick = {() => clean()}/>
-      {row1.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
-      <div>
-        {row2.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
-      </div>
-      <div>
-        {row3.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
-      </div>
-      <div>
-        {row4.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
-      </div>
-      <div>
-        {row5.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
-        <Button title = "=" onclick = {() => equals()}/>
-      </div>
-    </div>
+    <div className = "wrapper">
+        <div>
+        <input type="text" value={input + perInput}/>
+        </div>
+        <div>
+        <Button title = "AC" onclick = {() => clean()}/>
+        <Button title = "+/-" onclick = {() => addValue("/")}/>
+        <Button title = "%" onclick = {() => addValue("%")}/>
+        <Button title = "/" onclick = {() => addValue("/")}/>
+        </div>
+        <div>
+            {row2.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
+            <Button title = "X" onclick = {() => addValue("*")}/>
+        </div>
+        <div>
+            {row3.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
+            <Button title = "-" onclick = {() => addValue("-")}/>
+        </div>
+        <div>
+            {row4.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
+            <Button title = "+" onclick = {() => addValue("+")}/>
+        </div>
+        <div>
+            {row5.map((n) => <Button title = {n} onclick = {() => addValue(n)}/>)}
+            <Button title = "=" onclick = {() => equals()}/>
+        </div>  
     </div>
   )
 }
